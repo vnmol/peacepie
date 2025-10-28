721f941d6ec1508e45c9eb250de3a245b2085ef2
Vladimir Molodtsov <vmol@mail.ru>
2025-10-28 09:30:13 +0300

    ver0.2.3_for_railway_with_telegram

--- config/app_starter_simple_telegram_bot.py
+++ config/app_starter_simple_telegram_bot.py
@@ -22,8 +22,10 @@ class AppStarter:
     async def web_face(self):
         name = 'web_face'
         body = {'class_desc': {'requires_dist': 'simple_web_face', 'class': 'SimpleWebFace'}, 'name': name}
-        await self.adaptor.ask(self.adaptor.get_msg('create_actor', body), 10)
-        await self.adaptor.ask(self.adaptor.get_msg('start', {'port': 9090}, name), 10)
+        await self.adaptor.ask(self.adaptor.get_msg('create_actor', body), 60)
+        body = {'params': [{'name': 'http_port', 'value': 9090}]}
+        await self.adaptor.ask(self.adaptor.get_msg('set_params', body, name))
+        await self.adaptor.ask(self.adaptor.get_msg('start', None, name))
 
     async def telegram(self):
         # for i in range(3):
@@ -37,6 +39,6 @@ class AppStarter:
         name = 'telegram'
         class_desc = {'requires_dist': 'simple_telegram_bot', 'class': 'SimpleTelegramActor'}
         body = {'class_desc': class_desc, 'name': name}
-        await self.adaptor.ask(self.adaptor.get_msg('create_actor', body), 10)
+        await self.adaptor.ask(self.adaptor.get_msg('create_actor', body), 60)
         await self.adaptor.ask(self.adaptor.get_msg('start', None, name), 300)
         await self.adaptor.send(self.adaptor.get_msg('remove_actor', {'name': self.adaptor.name}))
